2e9b781725a1de524bf96f83aa227cdd4375b9d7
Ali Mansour <dev.ali.mansour@gmail.com>
2025-11-09 13:40:30 +0200

    feat: Provide CoroutineDispatcher for dependency injection
    Adds a `CoroutineDispatcher` to the Koin dependency graph to be used for I/O-bound operations.
    
    - Defines a `provideCoroutineDispatcher` method in `AppModule`.
    - Annotates the method with `@Single` to ensure a single instance of the dispatcher is created.
    - Configures the provider to return `Dispatchers.IO`.

--- app/src/main/java/dev/alimansour/mytasks/di/module/AppModule.kt
+++ app/src/main/java/dev/alimansour/mytasks/di/module/AppModule.kt
@@ -1,8 +1,14 @@
 package dev.alimansour.mytasks.di.module
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module(includes = [UseCaseModule::class])
 @ComponentScan("dev.alimansour.mytasks")
-class AppModule
+class AppModule {
+    @Single
+    fun provideCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
