7c84298e328625eefe25056fe12ce4d0465861ba
Ali Mansour <dev.ali.mansour@gmail.com>
2025-11-09 17:17:16 +0200

    refactor: Replace NavGraph with AppNavHost for better modularity
    Replaces the `NavGraph` composable with `AppNavHost` to decouple navigation from the main activity and improve screen modularity.
    
    - Renames `NavGraph.kt` to `AppNavHost.kt`.
    - `AppNavHost` now manages its own `navController`.
    - Introduces `onSetTopBar` and `onSetFab` lambda parameters to allow individual screens, like `HomeScreen`, to define their own top app bars and floating action buttons.
    - The `HomeScreen` composable is updated to accept these new parameters and navigation callbacks.

---
+++ app/src/main/java/dev/alimansour/mytasks/core/ui/navigation/AppNavHost.kt
@@ -0,0 +1,46 @@
+package dev.alimansour.mytasks.core.ui.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.alimansour.mytasks.feature.home.HomeScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier,
+    onSetTopBar: (@Composable () -> Unit) -> Unit,
+    onSetFab: (@Composable () -> Unit) -> Unit,
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Route.Home,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        composable<Route.Home> {
+            HomeScreen(
+                navigateToRoute = {
+                    navController.navigate(it)
+                },
+                navigateToTaskDetails = {
+                },
+                onSetTopBar = onSetTopBar,
+                onSetFab = onSetFab,
+                showError = {},
+            )
+        }
+    }
+}
+
+private fun NavHostController.navigateUpSafely() {
+    if (previousBackStackEntry == null) {
+        navigate(Route.Home)
+    } else {
+        navigateUp()
+    }
+}
