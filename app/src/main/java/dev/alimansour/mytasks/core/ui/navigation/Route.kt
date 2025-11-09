5037781fcada7dc4f5320b35e427cad393e6ccc8
Ali Mansour <dev.ali.mansour@gmail.com>
2025-11-09 11:43:21 +0200

    feat: Add navigation routes
    Introduces a sealed class `Route` to define the navigation destinations within the app. This class is serializable for type-safe navigation.
    
    - Defines `Home`, `NewTask`, and `TaskDetails` as the initial routes.

---
+++ app/src/main/java/dev/alimansour/mytasks/core/ui/navigation/Route.kt
@@ -0,0 +1,15 @@
+package dev.alimansour.mytasks.core.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Route {
+    @Serializable
+    data object Home : Route()
+
+    @Serializable
+    data object NewTask : Route()
+
+    @Serializable
+    data object TaskDetails : Route()
+}
