80fbc7104e86892d933eeb4827767346c3e0c4c9
Ali Mansour <dev.ali.mansour@gmail.com>
2025-11-09 17:02:16 +0200

    chore: Add Inter font family
    Adds the Inter variable font files for both normal and italic styles to the project resources.
    
    - Defines a new `interFamily` `FontFamily` in `Type.kt` to use these font files.
    - Cleans up commented-out code in `Theme.kt`.

--- app/src/main/java/dev/alimansour/mytasks/ui/theme/Theme.kt
+++ app/src/main/java/dev/alimansour/mytasks/ui/theme/Theme.kt
@@ -34,15 +34,6 @@ private val LightColorScheme =
         onSurfaceVariant = LightSecondaryText,
         // For less important text// For secondary text
         // You can fill in the rest (secondary, tertiary, error) as needed
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
     )
 
 @Composable
--- app/src/main/java/dev/alimansour/mytasks/ui/theme/Type.kt
+++ app/src/main/java/dev/alimansour/mytasks/ui/theme/Type.kt
@@ -2,19 +2,24 @@ package dev.alimansour.mytasks.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.alimansour.mytasks.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+val Typography =
+    Typography(
+        bodyLarge =
+            TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                letterSpacing = 0.5.sp,
+            ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -30,5 +35,11 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
-)
+     */
+    )
+
+val interFamily =
+    FontFamily(
+        Font(R.font.inter_variable_font, FontWeight.Normal),
+        Font(R.font.inter_italic_variable_font, style = FontStyle.Italic),
+    )
