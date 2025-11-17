c1e18aa4c9f9fe4e7f763ac0047a5fd6d89ea55d
Ali Mansour <dev.ali.mansour@gmail.com>
2025-11-17 11:53:49 +0200

    feat: Implement splash screen
    Adds a splash screen to the application using the `androidx.core:core-splashscreen` library.
    
    This includes:
    *   Adding the splash screen dependency to `gradle/libs.versions.toml` and `app/build.gradle.kts`.
    *   Implementing the splash screen logic in `MainActivity`, keeping it on screen for a short duration.
    *   Defining splash screen themes (`Theme.MyTasks.MySplash`) for both light and dark modes in `app/src/main/res/values/` and `app/src/main/res/values-night/`.
    *   Updating `AndroidManifest.xml` to use the new splash theme.
    *   Configuring `enableEdgeToEdge` with a transparent navigation bar style in `MainActivity`.

--- app/src/main/java/dev/alimansour/mytasks/MainActivity.kt
+++ app/src/main/java/dev/alimansour/mytasks/MainActivity.kt
@@ -1,15 +1,38 @@
 package dev.alimansour.mytasks
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import dev.alimansour.mytasks.core.ui.theme.MyTasksTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        var isLoadingSplash = true
+        lifecycleScope.launch {
+            delay(timeMillis = 1500L)
+            isLoadingSplash = false
+        }
+
+        installSplashScreen().setKeepOnScreenCondition {
+            isLoadingSplash
+        }
+
+        enableEdgeToEdge(
+            navigationBarStyle =
+                SystemBarStyle.light(
+                    Color.TRANSPARENT,
+                    Color.TRANSPARENT,
+                ),
+        )
+
         setContent {
             MyTasksTheme {
                 MainApp()
